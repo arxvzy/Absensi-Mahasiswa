9cdcc07a957a1ee4f4beb86d858bc2480ca76d85
Arxvzy <szaryabima@gmail.com>
2023-12-16 21:49:58 +0700

    merombak dan menambah susunan menu dan membuat menu dasar berfungsi

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ struct mahasiswa{
 };
 mahasiswa kelasa[40];
 int counter, pertemuan=1;
+
 void addMahasiswa(){
     cout << "\nNama Mahasiswa: \n";
     getline(cin, kelasa[counter].nama);
@@ -54,37 +55,107 @@ void addPresensi(){
     }
     pertemuan++;
 }
+
+void saveData(){
+    fstream extFile;
+    extFile.open("data.txt");
+    extFile << counter << endl;
+    if(extFile.is_open()){
+        for(int i=0; i<counter; i++){
+            extFile << kelasa[i].nama << endl;
+            extFile << kelasa[i].nim << endl;
+            extFile << kelasa[i].kehadiran << endl;
+            extFile << kelasa[i].izin << endl;
+            extFile << kelasa[i].sakit << endl;
+            extFile << kelasa[i].alpa << endl;
+        }
+        extFile.close();
+        cout << "\n\nData Berhasil Disimpan!\n\n";
+    } else {
+        cout << "\n\nFile Tidak Ditemukan!\n\n";
+    }
+}
+
+void loadData(){
+    fstream extFile;
+    extFile.open("data.txt");
+    if(extFile.is_open()){
+        extFile >> counter;
+        if(counter>0){
+            for(int i=0; i<counter; i++){
+                extFile >> kelasa[i].nama;
+                extFile >> kelasa[i].nim;
+                extFile >> kelasa[i].kehadiran;
+                extFile >> kelasa[i].izin;
+                extFile >> kelasa[i].sakit;
+                extFile >> kelasa[i].alpa;
+            }
+            extFile.close();
+            cout << "\n\nData Berhasil Dimuat!\n\n";
+        } else {
+            cout << "\n\nDatabase Kosong!\n\n";
+        }
+    } else {
+        cout << "\n\nFile Tidak Ditemukan!\n\n";
+    }
+}
 int main()
 {
-    char menuSelect;
+    char menuSelect, dashSelect;
     do{
         cout << "JUMLAH PERTEMUAN = " << pertemuan << endl;
+        cout << "JUMLAH MAHASISWA = " << counter << endl;
         cout << "======== MENU =======\n";
-        cout << "1. TAMBAHKAN DATA MAHASISWA\n";
-        cout << "2. PRESENSI MAHASISWA\n";
-        cout << "3. TAMPILKAN REKAP PRESENSI\n";
-        cout << "4. SIMPAN DATA\n";
+        cout << "1. DASHBOARD\n";
+        cout << "2. REKAP PRESENSI\n";
+        cout << "3. KELUAR\n";
         cout << "PILIH MENU = ";
-        menuSelect = getche();
+        cin >> menuSelect;
         cin.ignore();
         cout << "\n\n";
         switch(menuSelect){
         case '1':
-            addMahasiswa();
-            system("pause");
-            system("cls");
+            do{
+                cout << "======== DASHBOARD =======\n";
+                cout << "1. TAMBAH DATA MAHASISWA\n";
+                cout << "2. PRESENSI MAHASISWA\n";
+                cout << "3. SIMPAN DATA MAHASISWA\n";
+                cout << "4. MUAT DATA MAHASISWA\n";
+                cout << "5. KEMBALI\n";
+                cout << "PILIH MENU = ";
+                cin >> dashSelect;
+                cin.ignore();
+                cout << "\n\n";
+
+                if(dashSelect == '1'){
+                    addMahasiswa();
+                    system("pause");
+                    system("cls");
+                } else if(dashSelect == '2'){
+                    addPresensi();
+                    system("pause");
+                    system("cls");
+                } else if(dashSelect == '3'){
+                    saveData();
+                    system("pause");
+                    system("cls");
+                } else if(dashSelect == '4'){
+                    loadData();
+                    system("pause");
+                    system("cls");
+                } else if(dashSelect == '5'){
+                    break;
+                }
+            } while(true);
             break;
         case '2':
-            addPresensi();
-            system("pause");
-            system("cls");
-            break;
-        case '3':
             showRekap();
             system("pause");
             system("cls");
             break;
+        case '3':
+            exit(0);
         }
-    } while (menuSelect != 27);
+    } while (true);
     return 0;
 }
